e842a37e65aeb5b67d5d281fc15b102504a79401
eliezerrb <eliezerrezende@outlook.com>
2022-03-02 11:17:10 -0300

    Payment, one-to-one association

--- src/main/java/com/eliezer/course/config/TestConfig.java
+++ src/main/java/com/eliezer/course/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.eliezer.course.entities.Category;
 import com.eliezer.course.entities.Order;
 import com.eliezer.course.entities.OrderItem;
+import com.eliezer.course.entities.Payment;
 import com.eliezer.course.entities.Product;
 import com.eliezer.course.entities.User;
 import com.eliezer.course.entities.enums.OrderStatus;
@@ -85,6 +86,10 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
